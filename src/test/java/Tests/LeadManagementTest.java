36eaec598b1a167401edb5c69ea1c72be684c0f1
gopalBariyaqb03 <gopalbariyaqb03@gmail.com>
2025-12-19 13:05:16 +0530

    Delete idia and allure report files

--- src/test/java/Tests/LeadManagementTest.java
+++ src/test/java/Tests/LeadManagementTest.java
@@ -10,10 +10,8 @@ public class LeadManagementTest extends BasePage {
         lead.VerifyCreateNewLead();
     }
 
-
     @Test
     public void VerifycreateLeadSearch() {
-
         loginWithAdminUser();
         String CLead = lead.VerifyCreateNewLeadandreturnname().toString();
         lead.VerifyCreateLeadAndSearch(CLead);
@@ -48,7 +46,6 @@ public class LeadManagementTest extends BasePage {
 
     @Test
     public void verifyEditFunctionalityOnLeadManagementPage() {
-
         common.logPrint("Verify Edit Functionality on Lead Management page");
         loginWithAdminUser();
         lead.VerifyUpdateLead();
